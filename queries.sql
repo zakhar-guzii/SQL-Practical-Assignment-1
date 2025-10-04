464c69ef913e68bd46b8b7862651648bc9fca21c
Zakhar Guzii <zakharguzii@gmail.com>
2025-10-05 00:14:07 +0300

    just finished some tasks

--- queries.sql
+++ queries.sql
@@ -31,5 +31,35 @@ select order_id, customer_id, order_date, status
 from orders
 where status = 'Canceled';
 
--- CTE
+-- CTEs, order by, where
+
+-- Знайти покупців, що входить в топ 30% за витраченими коштами
+with cust_totals as(
+    select customer_id, sum(total_amount) as total_spend,
+    count(*) as orders_count
+    from orders
+    group by customer_id
+),
+best_buyers as(
+    select customer_id,
+           total_spend,
+           orders_count,
+           percent_rank() over (order by total_spend desc) as rp
+    from cust_totals
+)
+select
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.email,
+    bb.total_spend,
+    bb.orders_count
+from customers c
+inner join best_buyers bb
+on bb.customer_id = c.customer_id
+where rp <=0.3
+order by bb.total_spend desc;
+
+-- subquery
+
 
