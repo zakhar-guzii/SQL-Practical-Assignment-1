07dfcc442c90c2931248ca2abbde414d03ac87a1
Zakhar Guzii <zakharguzii@gmail.com>
2025-10-05 13:31:29 +0300

    probably i done it, just need to write comments, add normal read me file, and prepare to theory

--- create_tables.sql
+++ create_tables.sql
@@ -1,28 +1,30 @@
 CREATE DATABASE ecommerce_db;
 use ecommerce_db;
 
-create table customers(
-    customer_id int primary key AUTO_INCREMENT,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(100) unique,
-    phone varchar(20),
-    city varchar(50),
-    creation_time date,
+create table customers
+(
+    customer_id    int primary key AUTO_INCREMENT,
+    first_name     varchar(50),
+    last_name      varchar(50),
+    email          varchar(100) unique,
+    phone          varchar(20),
+    city           varchar(50),
+    creation_time  date,
     loyalty_points int
 );
 
-create table orders(
-    order_id int primary key AUTO_INCREMENT,
-    customer_id int,
-    foreign key (customer_id) references customers(customer_id),
-    order_date date,
-    status enum(
+create table orders
+(
+    order_id         int primary key AUTO_INCREMENT,
+    customer_id      int,
+    foreign key (customer_id) references customers (customer_id),
+    order_date       date,
+    status           enum (
         'Pending',
         'Shipped',
         'Delivered',
         'Canceled'),
-    payment_method enum(
+    payment_method   enum (
         'Credit Card',
         'Debit Card',
         'Cash',
@@ -31,38 +33,41 @@ create table orders(
         'Bank Transfer'
         ),
     shipping_address varchar(200),
-    total_amount decimal(10,2)
+    total_amount     decimal(10, 2)
 );
 
-create table suppliers(
-    supplier_id int primary key AUTO_INCREMENT,
-    name varchar(100),
+create table suppliers
+(
+    supplier_id  int primary key AUTO_INCREMENT,
+    name         varchar(100),
     contact_name varchar(100),
-    email varchar(100),
-    phone varchar(20),
-    country varchar(50),
-    rating decimal(3,2)
+    email        varchar(100),
+    phone        varchar(20),
+    country      varchar(50),
+    rating       decimal(3, 2)
 );
 
-create table products(
-    product_id int primary key AUTO_INCREMENT,
-    name varchar(100),
-    description text,
-    price decimal(10,2),
-    category varchar(50),
+create table products
+(
+    product_id     int primary key AUTO_INCREMENT,
+    name           varchar(100),
+    description    text,
+    price          decimal(10, 2),
+    category       varchar(50),
     stock_quantity int,
-    supplier_id int,
-    foreign key (supplier_id) references suppliers(supplier_id),
-    created_at date
+    supplier_id    int,
+    foreign key (supplier_id) references suppliers (supplier_id),
+    created_at     date
 );
 
-create table order_items(
+create table order_items
+(
     order_item_id int primary key AUTO_INCREMENT,
-    order_id int references orders(order_id),
-    foreign key (order_id) references orders(order_id),
-    product_id int references products(product_id),
-    foreign key (product_id) references products(product_id),
-    quantity int,
-    unit_price decimal(10,2),
-    discount decimal(5,2)
+    order_id      int references orders (order_id),
+    foreign key (order_id) references orders (order_id),
+    product_id    int references products (product_id),
+    foreign key (product_id) references products (product_id),
+    quantity      int,
+    unit_price    decimal(10, 2),
+    discount      decimal(5, 2)
 );
--- queries.sql
+++ queries.sql
@@ -2,22 +2,22 @@ use ecommerce_db;
 
 -- 1. select joining 5 tables
 
-select
-    c.first_name,
-    c.last_name,
-    p.name as product_name,
-    oi.quantity,
-    oi.unit_price,
-    (oi.quantity * oi.unit_price) as total_item_amount,
-    s.name as supplier_name,
-    s.rating as supplier_rating,
-    o.order_date,
-    o.status
+-- Отримати повну інформацію про все
+select c.first_name,
+       c.last_name,
+       p.name                        as product_name,
+       oi.quantity,
+       oi.unit_price,
+       (oi.quantity * oi.unit_price) as total_item_amount,
+       s.name                        as supplier_name,
+       s.rating                      as supplier_rating,
+       o.order_date,
+       o.status
 from customers c
-join orders o on c.customer_id = o.customer_id
-join order_items oi on o.order_id = oi.order_id
-join products p on oi.product_id = p.product_id
-join suppliers s on p.supplier_id = s.supplier_id
+         join orders o on c.customer_id = o.customer_id
+         join order_items oi on o.order_id = oi.order_id
+         join products p on oi.product_id = p.product_id
+         join suppliers s on p.supplier_id = s.supplier_id
 order by o.order_date desc;
 
 -- addition: union
@@ -25,7 +25,7 @@ order by o.order_date desc;
 -- Таблиця замовлень, потребуючих додаткової уваги
 select order_id, customer_id, order_date, status
 from orders
-where status  = 'Pending'
+where status = 'Pending'
 union
 select order_id, customer_id, order_date, status
 from orders
@@ -34,32 +34,113 @@ where status = 'Canceled';
 -- CTEs, order by, where
 
 -- Знайти покупців, що входить в топ 30% за витраченими коштами
-with cust_totals as(
-    select customer_id, sum(total_amount) as total_spend,
-    count(*) as orders_count
-    from orders
-    group by customer_id
-),
-best_buyers as(
-    select customer_id,
-           total_spend,
-           orders_count,
-           percent_rank() over (order by total_spend desc) as rp
-    from cust_totals
-)
-select
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    c.email,
-    bb.total_spend,
-    bb.orders_count
+with cust_totals as (select customer_id,
+                            sum(total_amount) as total_spend,
+                            count(*)          as orders_count
+                     from orders
+                     group by customer_id),
+     best_buyers as (select customer_id,
+                            total_spend,
+                            orders_count,
+                            percent_rank() over (order by total_spend desc) as rp
+                     from cust_totals)
+select c.customer_id,
+       c.first_name,
+       c.last_name,
+       c.email,
+       bb.total_spend,
+       bb.orders_count
 from customers c
-inner join best_buyers bb
-on bb.customer_id = c.customer_id
-where rp <=0.3
+         inner join best_buyers bb
+                    on bb.customer_id = c.customer_id
+where rp <= 0.3
 order by bb.total_spend desc;
 
--- subquery
 
+-- subquery, group by & having
 
+-- Найкращий постачальник міста
+select c.city,
+       s.name           as supplier_name,
+       sum(oi.quantity) as total_items
+from customers c
+         inner join orders o on o.customer_id = c.customer_id
+         inner join order_items oi on oi.order_id = o.order_id
+         inner join products p on p.product_id = oi.product_id
+         inner join suppliers s on s.supplier_id = p.supplier_id
+group by c.city, s.supplier_id
+having sum(oi.quantity) = (select max(total_per_supplier)
+                           from (select sum(oi2.quantity) as total_per_supplier
+                                 from customers c2
+                                          join orders o2 on o2.customer_id = c2.customer_id
+                                          join order_items oi2 on oi2.order_id = o2.order_id
+                                          join products p2 on p2.product_id = oi2.product_id
+                                 where c2.city = c.city
+                                 group by p2.supplier_id) as sub)
+order by c.city;
+
+-- king of queries
+
+-- знайти топ клієнтів з найбільшими витрататами за останні 6 місяців,
+-- разом з інформацією про те, що вони замовили за цей час,
+-- найбільш популярний постачальників та категорії товарів
+
+with recent_orders as (select o.order_id, -- всі замовлення, окрім скасованих
+                              o.customer_id,
+                              o.total_amount,
+                              o.order_date,
+                              o.status
+                       from orders o
+                       where o.order_date >= curdate() - interval 6 month
+                         and o.status != 'Canceled'),
+     customer_spending as (select r.customer_id, -- загальна сума витрат
+                                  sum(r.total_amount) as total_spent,
+                                  count(r.order_id)   as total_orders
+                           from recent_orders r
+                           group by r.customer_id)
+    (select c.customer_id,
+            concat(c.first_name, '', c.last_name) as full_name,
+            c.email,
+            cs.total_spent,
+            cs.total_orders,
+            o.order_id,
+            o.order_date,
+            p.name                                as product_name,
+            p.category,
+            s.name                                as supplier_name,
+            (select s2.name
+             from order_items oi2
+                      join products p2 on oi2.product_id = p2.product_id
+                      join suppliers s2 on p2.supplier_id = s2.supplier_id
+                      join orders o2 on oi2.order_id = o2.order_id
+             where o2.customer_id = c.customer_id
+             group by s2.supplier_id
+             order by count(*) desc
+             limit 1)                             as top_supplier
+     from customers c
+              join customer_spending cs on c.customer_id = cs.customer_id
+              join orders o on c.customer_id = o.customer_id
+              join order_items oi on o.order_id = oi.order_id
+              join products p on oi.product_id = p.product_id
+              join suppliers s on p.supplier_id = s.supplier_id
+     where cs.total_spent > 1000
+     group by c.customer_id
+     having count(distinct o.order_id) >= 2)
+union all
+(select c.customer_id,
+        concat(c.first_name, ' ', c.last_name) as full_name,
+        c.email,
+        cs.total_spent,
+        cs.total_orders,
+
+        null                                   as order_id,
+        null                                   as order_date,
+        null                                   as product_name,
+        null                                   as category,
+        null                                   as supplier_name,
+        null                                   as top_supplier
+ from customers c
+          join customer_spending cs on c.customer_id = cs.customer_id
+ where cs.total_orders = 1)
+order by total_spent desc
+limit 15;
